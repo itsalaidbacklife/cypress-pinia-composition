475ef5c1537fecfedaf5033809b00865359a4bc7
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-05-24 11:39:25 -0400

    test: add failing test case for reading the pinia store using the useCounterStore() function

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,23 @@
-
+import { useCounterStore } from '../../src/stores/counter';
 
 describe('My First Test', () => {
   beforeEach(() => {
     cy.visit('/');
   });
+
   it('Reads the DOM', () => {
     cy.get('[data-cy=count]').contains('The count is 0');
     cy.get('[data-cy=increment]').click();
     cy.get('[data-cy=count]').contains('The count is 1');
+  });
+
+  // I can't figure out how to read the pinia store itself
+  it('Fails to read the pinia store using the useCounterStore() helper', () => {
+    cy.log('Test Initial count');
+    // Can't call useCounterStore() without pinia gloabal pinia instance
+    cy.wrap(useCounterStore())
+      .then((counterStore) => {
+        expect(counterStore.counter).to.eq(0);
+      });
   });
 })
